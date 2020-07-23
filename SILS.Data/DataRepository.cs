322d24c1391b3ccb189be10e3c39ae7e8a995414
sdw932001@naver.com <sdw932001@naver.com>
2020-07-23 11:01:01 +0900

    libraryinsert

---
+++ SILS.Data/DataRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SILS.Data
+{
+    public class DataRepository
+    {
+        public static CodeData Code { get; } = new CodeData();
+        public static BookData Book { get; } = new BookData();
+        public static LibraryData Library { get; } = new LibraryData();
+        public static HoldingListData HoldingList { get; } = new HoldingListData();
+    }
+}
